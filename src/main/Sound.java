1defec6cc0004181276b7308e372bede6c4dfec8
Pei-Wen Tang (Vincent) <redjackfred@gmail.com>
2024-08-15 14:09:58 -0700

    Level up

--- src/main/Sound.java
+++ src/main/Sound.java
@@ -18,6 +18,7 @@ public class Sound {
         soundURL[4] = getClass().getResource("/sound/fanfare.wav");
         soundURL[5] = getClass().getResource("/sound/hitmonster.wav");
         soundURL[6] = getClass().getResource("/sound/receivedamage.wav");
+        soundURL[8] = getClass().getResource("/sound/levelup.wav");
     }
 
     public void setFile(int i){
